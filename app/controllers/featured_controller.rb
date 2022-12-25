75e2f992836c1cdf078ce43a9fdff94d124c8bb3
Bbosa Muhamood <44940073+bmuhamood@users.noreply.github.com>
2022-12-25 18:20:59 +0400

    completed featured feature and related CSS on the home page

--- app/controllers/featured_controller.rb
+++ app/controllers/featured_controller.rb
@@ -1,5 +1,7 @@
 class FeaturedController < ApplicationController
   def home
     @featured_offers = Offer.where(featured: true)
+    @featured_places = Place.where(featured: true)
+    @featured_coupans = Coupan.where(featured: true)
   end
 end
